d65de08bd8b9468f3b9330c1171c7ac62812db4b
urrickhunt <96319944+urrickhunt@users.noreply.github.com>
2023-03-24 08:43:58 -0500

    Add files via upload

---
+++ DraculaPro.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#FF9580", "#8AFF80", "#FFFF80", "#9580FF", "#FF80BF", "#80FFEA", "#bbbbbb", "#555555", "#FF9580", "#8AFF80", "#FFFF80", "#9580FF", "#FF80BF", "#80FFEA", "#ffffff"]);
+t.prefs_.set('foreground-color', "#f8f8f2");
+t.prefs_.set('background-color', "#22212C");
+t.prefs_.set('cursor-color', 'rgba(149,128,255,0.5)');
